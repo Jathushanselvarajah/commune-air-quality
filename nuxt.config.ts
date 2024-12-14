5dbf4cd2ac48bfb5f8a8c6c3b933bab18eb81a79
Jathushan Selvarajah <126668649+Jathushanselvarajah@users.noreply.github.com>
2024-12-14 14:08:44 +0100

    Update SEO in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,34 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Commune Details 📍",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Découvrez les informations détaillées sur les communes en France, incluant le code postal, la population, et la qualité de l’air avec des cartes interactives.",
+        },
+        {
+          name: "keywords",
+          content:
+            "communes, France, carte interactive, AQI, qualité de l’air, Vue.js, Leaflet, API",
+        },
+        {
+          property: "og:title",
+          content: "Commune Details 📍",
+        },
+        {
+          property: "og:description",
+          content:
+            "Explorez les informations détaillées sur les communes en France avec une carte interactive et des données sur la qualité de l’air.",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+      ],
+    },
+  },
 });
